df207c87082134285feb52361b432cd351ecf77b
Heiya <morooka.akira.inon29@gmail.com>
2018-07-14 18:17:27 +0900

    add app config.

--- src/util/connectors.js
+++ src/util/connectors.js
@@ -1,4 +1,8 @@
-import { Connect } from 'uport-connect'
+import { Connect, SimpleSigner } from 'uport-connect'
 
-export let uport = new Connect('TruffleBox')
+export const uport = new Connect('sample app', {
+  clientId: '2om8QVhSgPkChNGGRAKNtKYcDbEeoych8ht',
+  network: 'rinkeby',
+  signer: SimpleSigner('f5adf841d6a4659961c1478a4670a5f3e54e61c1c55f4e33424c9f1ce1b714ea')
+})
 export const web3 = uport.getWeb3()
